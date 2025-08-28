bac6f751e15521f9ee96efbbba1f6f3224c08168
aleishis <mecedesval@gmail.com>
2025-08-28 10:34:02 -0700

    Added rows with fahrenheit temperature measurements, and a method that converts celsius to fahrenheit

---
+++ src/main/java/mx/itson/prime/business/Palindrome.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.prime.business;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alelo
+ */
+public class Palindrome {
+    
+    public static boolean isPalindrome (String value){
+        
+        value = value.replaceAll("\\s", "");
+        char[] letras = value.toCharArray();
+        
+        
+        int counter = letras.length;
+        for (char letra : letras){
+         
+            if (letra != letras[counter - 1]){
+                return false;
+            }
+            counter--;
+            if (counter < 0){
+                return true;
+            }
+        }
+        
+        return true;
+    }
+}
